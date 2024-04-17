a9aece008d71e09e785be7888939838332ee63ad
YunJiW <dtroy371@gmail.com>
2024-04-17 15:30:30 +0900

    feat : add AnswerEntity

--- src/main/java/com/mysite/sbb/Answer.java
+++ src/main/java/com/mysite/sbb/Answer.java
@@ -5,27 +5,25 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 import static jakarta.persistence.GenerationType.*;
 
 @Getter
 @Setter
 @Entity
-public class Question {
+public class Answer {
 
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Integer id;
-
-    @Column(length = 200)
-    private String subject;
 
     @Column(columnDefinition = "TEXT")
     private String content;
 
     private LocalDateTime createDate;
 
-    @OneToMany(mappedBy = "question",cascade = CascadeType.REMOVE)
-    private List<Answer> answerList;
+
+    @ManyToOne
+    private Question question;
+
 }
