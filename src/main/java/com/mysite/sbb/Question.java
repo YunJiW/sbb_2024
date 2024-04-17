35d6625e0e76586f9e7dfbabc7a7cdc916d22d17
YunJiW <dtroy371@gmail.com>
2024-04-17 15:26:01 +0900

    feat : Add QuestionEntity

---
+++ src/main/java/com/mysite/sbb/Question.java
@@ -0,0 +1,25 @@
+package com.mysite.sbb;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+}
