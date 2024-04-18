b4847678e9362e996fd3294cccf9c61bcd35811c
YunJiW <dtroy371@gmail.com>
2024-04-18 19:04:07 +0900

    feat : DataNotFoundException and update QuestionService

--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -1,9 +1,11 @@
 package com.mysite.sbb.question;
 
+import com.mysite.sbb.exception.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -13,5 +15,14 @@ public class QuestionService {
 
     public List<Question> getList() {
         return questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id){
+        Optional<Question> findQuestion = questionRepository.findById(id);
+        if(findQuestion.isPresent()){
+            return findQuestion.get();
+        }else{
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
