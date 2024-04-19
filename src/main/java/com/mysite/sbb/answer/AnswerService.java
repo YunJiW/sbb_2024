20241a0aae00374caac38d451e3cea1b4429d2c3
JIWOO YUN <54807231+YunJiW@users.noreply.github.com>
2024-04-19 13:26:15 +0900

    Answer (#5)
    * feat : add AnswerService and AnswerRepository
    
    * add style.css

---
+++ src/main/java/com/mysite/sbb/answer/AnswerService.java
@@ -0,0 +1,24 @@
+package com.mysite.sbb.answer;
+
+import com.mysite.sbb.question.Question;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class AnswerService {
+
+    private final AnswerRepository answerRepository;
+
+    public void create(Question question, String content) {
+        Answer answer = new Answer();
+        answer.setContent(content);
+        answer.setQuestion(question);
+        answer.setCreateDate(LocalDateTime.now());
+
+        answerRepository.save(answer);
+    }
+
+}
