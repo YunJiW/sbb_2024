1a709ffc4a4e1583323d6af390630d0ffb53fa34
YunJiW <dtroy371@gmail.com>
2024-04-19 17:02:45 +0900

    feat : Add Question Form

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,9 +1,14 @@
 package com.mysite.sbb.question;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
@@ -30,13 +35,17 @@ public class QuestionController {
     }
 
     @GetMapping("/create")
-    public String questionCreate() {
+    public String questionCreate(QuestionForm questionForm) {
         return "question_form";
     }
 
     @PostMapping("/create")
-    public String questionCreate(@RequestParam(value = "subject") String subject,
-                                 @RequestParam(value = "content") String content) {
+    public String questionCreate(@Valid QuestionForm questionForm,
+                                 BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "question_form";
+        }
+        questionService.create(questionForm.getSubject(), questionForm.getContent());
         return "redirect:/question/list";
     }
 
---
+++ src/main/java/com/mysite/sbb/question/QuestionForm.java
@@ -0,0 +1,19 @@
+package com.mysite.sbb.question;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class QuestionForm {
+
+    @NotEmpty(message = "제목은 필수 항목입니다.")
+    @Size(max = 50)
+    private String subject;
+
+
+    @NotEmpty(message = "내용은 필수입니다.")
+    private String content;
+}
