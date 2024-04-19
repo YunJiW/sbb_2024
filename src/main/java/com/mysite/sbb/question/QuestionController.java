7bce7ce07832eb816c597e7fccc63395f3fd8ba3
YunJiW <dtroy371@gmail.com>
2024-04-19 17:30:00 +0900

    feat :Add AnswerForm

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,5 +1,6 @@
 package com.mysite.sbb.question;
 
+import com.mysite.sbb.answer.AnswerForm;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -28,7 +29,8 @@ public class QuestionController {
     }
 
     @GetMapping("/detail/{id}")
-    public String detail(Model model, @PathVariable("id") Integer id) {
+    public String detail(Model model, @PathVariable("id") Integer id,
+                         AnswerForm answerForm) {
         Question question = questionService.getQuestion(id);
         model.addAttribute("question", question);
         return "question_detail";
