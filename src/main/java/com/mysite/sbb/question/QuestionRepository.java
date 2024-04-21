9ff3096bea31350a19952060240a0226c622b0a4
YunJiW <dtroy371@gmail.com>
2024-04-21 17:53:53 +0900

    add Pageable

--- src/main/java/com/mysite/sbb/question/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/question/QuestionRepository.java
@@ -1,8 +1,18 @@
 package com.mysite.sbb.question;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface QuestionRepository extends JpaRepository<Question,Integer> {
+import java.util.List;
+
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
 
     Question findBySubject(String subject);
+
+    Question findBySubjectAndContent(String subject, String content);
+
+    List<Question> findBySubjectLike(String subject);
+
+    Page<Question> findAll(Pageable pageable);
 }
--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,13 +1,24 @@
 package com.mysite.sbb;
 
+import com.mysite.sbb.question.QuestionService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SbbApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private QuestionService questionService;
 
+
+	@Test
+	void testJpa(){
+
+		for(int idx =1; idx <=300;idx++){
+			String subject = String.format("테스트 데이터 입니다:[%03d]",idx);
+			String content = "내용무";
+			questionService.create(subject,content);
+		}
+	}
 }
