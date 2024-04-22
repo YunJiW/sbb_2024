8bda2ceff33fd3dff83a65a424c6f6274aec1c69
YunJiW <dtroy371@gmail.com>
2024-04-22 17:59:57 +0900

    create UserRepo and UserService

---
+++ src/main/java/com/mysite/sbb/user/UserService.java
@@ -0,0 +1,23 @@
+package com.mysite.sbb.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public SiteUser create(String username,String email,String password){
+        SiteUser user = new SiteUser();
+        user.setUsername(username);
+        user.setEmail(email);
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        user.setPassword(passwordEncoder.encode(password));
+        userRepository.save(user);
+
+        return user;
+    }
+}
