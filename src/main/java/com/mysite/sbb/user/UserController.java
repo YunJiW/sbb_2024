6e87ddca5afeb7345d44464ba0fafea1bf895206
YunJiW <dtroy371@gmail.com>
2024-04-22 19:06:24 +0900

    중복회원 가입 방지시 500에러 안나게 예외처리 추가

--- src/main/java/com/mysite/sbb/user/UserController.java
+++ src/main/java/com/mysite/sbb/user/UserController.java
@@ -2,6 +2,7 @@ package com.mysite.sbb.user;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,25 +17,34 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/signup")
-    public String signup(UserCreateForm userCreateForm){
+    public String signup(UserCreateForm userCreateForm) {
         return "signup_form";
     }
 
     @PostMapping("/signup")
     public String signup(@Valid UserCreateForm userCreateForm,
-                         BindingResult bindingResult){
-        if(bindingResult.hasErrors()){
+                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
             return "signup_form";
         }
 
-        if(!userCreateForm.getPassword1().equals(userCreateForm.getPassword2())){
-            bindingResult.rejectValue("password2","passwordIncorrect",
+        if (!userCreateForm.getPassword1().equals(userCreateForm.getPassword2())) {
+            bindingResult.rejectValue("password2", "passwordIncorrect",
                     "2개의 패스워드가 일치하지 않습니다.");
             return "signup_form";
         }
 
-        userService.create(userCreateForm.getUsername(),userCreateForm.getEmail(),userCreateForm.getPassword1());
+        try {
+            userService.create(userCreateForm.getUsername(), userCreateForm.getEmail(), userCreateForm.getPassword1());
+        } catch (DataIntegrityViolationException e) {
+            bindingResult.rejectValue("signupFailed", "이미 등록된 사용자입니다.");
 
+            return "signup_form";
+        } catch (Exception e) {
+            e.printStackTrace();
+            bindingResult.rejectValue("signupFailed", e.getMessage());
+            return "signup_form";
+        }
         return "redirect:/";
     }
 }
