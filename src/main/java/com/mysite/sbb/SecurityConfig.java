5ab37638a562d3420351b088fecc8f41fd084545
YunJiW <dtroy371@gmail.com>
2024-04-23 15:57:54 +0900

    add AuthenticationManager

--- src/main/java/com/mysite/sbb/SecurityConfig.java
+++ src/main/java/com/mysite/sbb/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.mysite.sbb;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -22,14 +24,20 @@ public class SecurityConfig {
                         .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
                 .formLogin((formLogin) -> formLogin.loginPage("/user/login")
                         .defaultSuccessUrl("/"))
-                ;
+        ;
 
         return http.build();
     }
 
     @Bean
-    BCryptPasswordEncoder passwordEncoder(){
+    BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
 
--- src/main/java/com/mysite/sbb/user/UserRole.java
+++ src/main/java/com/mysite/sbb/user/UserRole.java
@@ -1,5 +1,8 @@
 package com.mysite.sbb.user;
 
+import lombok.Getter;
+
+@Getter
 public enum UserRole {
     ADMIN("ROLE_ADMIN"),
     USER("ROLE_USER");
