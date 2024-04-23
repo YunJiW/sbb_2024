68688a15468d3308133380166ecf137c1c5fa10b
YunJiW <dtroy371@gmail.com>
2024-04-23 13:02:26 +0900

    SecurityConfig add formLogin

--- src/main/java/com/mysite/sbb/SecurityConfig.java
+++ src/main/java/com/mysite/sbb/SecurityConfig.java
@@ -20,6 +20,8 @@ public class SecurityConfig {
         http.
                 authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
                         .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
+                .formLogin((formLogin) -> formLogin.loginPage("/user/login")
+                        .defaultSuccessUrl("/"))
                 ;
 
         return http.build();
