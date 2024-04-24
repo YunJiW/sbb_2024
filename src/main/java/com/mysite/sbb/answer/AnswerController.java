50b98c3e992043fbd0a544663973fd7994d5a3cc
YunJiW <dtroy371@gmail.com>
2024-04-24 16:38:13 +0900

    AnswerController: add answerDelete

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -8,7 +8,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -50,7 +49,7 @@ public class AnswerController {
 
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/modify/{id}")
-    public String answerModify(AnswerForm answerForm,@PathVariable("id")Integer id,Principal principal){
+    public String answerModify(AnswerForm answerForm, @PathVariable("id") Integer id, Principal principal) {
 
         Answer answer = answerService.getAnser(id);
         if (!answer.getAuthor().getUsername().equals(principal.getName())) {
@@ -63,9 +62,9 @@ public class AnswerController {
 
     @PreAuthorize("isAuthenticated()")
     @PostMapping("/modify/{id}")
-    public String answerModify(@Valid AnswerForm answerForm,BindingResult bindingResult,
-                               @PathVariable("id")Integer id,Principal principal){
-        if(bindingResult.hasErrors()){
+    public String answerModify(@Valid AnswerForm answerForm, BindingResult bindingResult,
+                               @PathVariable("id") Integer id, Principal principal) {
+        if (bindingResult.hasErrors()) {
             return "answer_form";
         }
 
@@ -73,8 +72,22 @@ public class AnswerController {
         if (!answer.getAuthor().getUsername().equals(principal.getName())) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
         }
-        answerService.modify(answer,answerForm.getContent());
-        return String.format("redirect:/question/detail/%s",answer.getQuestion().getId());
+        answerService.modify(answer, answerForm.getContent());
+        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/delete/{id}")
+    public String answerDelete(Principal principal, @PathVariable("id") Integer id) {
+        Answer answer = answerService.getAnser(id);
+
+        if (!answer.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+
+        answerService.delete(answer);
+
+        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
     }
 
 }
