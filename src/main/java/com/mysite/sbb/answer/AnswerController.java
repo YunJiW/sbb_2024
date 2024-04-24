9c57507c723e116c8f861bf89dc7ab638e0fd89d
YunJiW <dtroy371@gmail.com>
2024-04-24 16:29:55 +0900

    AnswerController : Add answerModify PostMapping

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -8,6 +8,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -59,4 +60,21 @@ public class AnswerController {
         answerForm.setContent(answer.getContent());
         return "answer_form";
     }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/modify/{id}")
+    public String answerModify(@Valid AnswerForm answerForm,BindingResult bindingResult,
+                               @PathVariable("id")Integer id,Principal principal){
+        if(bindingResult.hasErrors()){
+            return "answer_form";
+        }
+
+        Answer answer = answerService.getAnser(id);
+        if (!answer.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        answerService.modify(answer,answerForm.getContent());
+        return String.format("redirect:/question/detail/%s",answer.getQuestion().getId());
+    }
+
 }
