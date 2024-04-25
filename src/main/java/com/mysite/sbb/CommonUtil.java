ce82597c5783edd0d8462157c54b40fa088177ea
YunJiW <dtroy371@gmail.com>
2024-04-25 13:59:32 +0900

    feat : CommonUtil

---
+++ src/main/java/com/mysite/sbb/CommonUtil.java
@@ -0,0 +1,18 @@
+package com.mysite.sbb;
+
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommonUtil {
+
+    public String markdown(String markdown) {
+        Parser parser = Parser.builder().build();
+        Node doc = parser.parse(markdown);
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+
+        return renderer.render(doc);
+    }
+}
