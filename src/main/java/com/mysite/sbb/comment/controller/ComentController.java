dd204b807d59dff8ba399b635c170d71e8af7a63
YunJiW <dtroy371@gmail.com>
2024-05-04 19:30:41 +0900

    setting : comment repo,service,controller setting

---
+++ src/main/java/com/mysite/sbb/comment/controller/ComentController.java
@@ -0,0 +1,9 @@
+package com.mysite.sbb.comment.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class ComentController {
+}
