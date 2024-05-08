1aee2703f49836863e7f185f4b5d04981fe6af72
JIWOO YUN <54807231+YunJiW@users.noreply.github.com>
2024-05-08 21:39:08 +0900

    Enhancement/1 (#15)
    * 프로필 기본 구성
    
    * 질문 list 타임리프 먼저 추가
    
    * 내가 쓴 정답 리스트까지 구현 페이징은 아직 구현안됨
    
    * 내정보 추가 username email
    
    * feat : 내가 작성한 질문ã 페이징 완료
    
    * chore : 링크 색상및 밑줄 제거
    
    * 내가 적은 답변 페이징 추가

--- src/main/java/com/mysite/sbb/user/controller/UserController.java
+++ src/main/java/com/mysite/sbb/user/controller/UserController.java
@@ -1,16 +1,27 @@
 package com.mysite.sbb.user.controller;
 
+import com.mysite.sbb.answer.Answer;
+import com.mysite.sbb.answer.service.AnswerService;
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.service.QuestionService;
+import com.mysite.sbb.user.SiteUser;
 import com.mysite.sbb.user.UserCreateForm;
 import com.mysite.sbb.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.security.Principal;
 
 @RequiredArgsConstructor
 @Controller
@@ -19,6 +30,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     private final UserService userService;
+
+    private final QuestionService questionService;
+
+    private final AnswerService answerService;
 
     @GetMapping("/signup")
     public String signup(UserCreateForm userCreateForm) {
@@ -56,8 +71,22 @@ public class UserController {
     }
 
     @GetMapping("/login")
-    public String login(){
+    public String login() {
         return "login_form";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/me")
+    public String me(Model model, @RequestParam(value = "page", defaultValue = "0") int page,
+                     @RequestParam(value = "page2", defaultValue = "0") int page2, Principal principal) {
+        SiteUser user = userService.getUser(principal.getName());
+
+        Page<Question> paging = questionService.getUserQuestionList(page, user.getId());
+        Page<Answer> answerPage = answerService.getUserAnswerList(page2, user.getId());
+        model.addAttribute("SiteUser", user);
+        model.addAttribute("paging", paging);
+        model.addAttribute("answerPaging", answerPage);
+        return "member_me";
     }
 
 }
